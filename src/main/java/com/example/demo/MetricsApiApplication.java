70c66e58d495c456cafbfa046042c11647a03b67
Nick Paolella <nickpaolella45@gmail.com>
2019-08-14 11:03:07 -0400

    Created Spring Boot Application

---
+++ src/main/java/com/example/demo/MetricsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsApiApplication.class, args);
+	}
+
+}
