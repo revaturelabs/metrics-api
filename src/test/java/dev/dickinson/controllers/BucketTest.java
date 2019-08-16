44b5fb9270945a0b8dc24225356b59cf11151e7b
WilliamDickinson <49538385+WilliamDickinson@users.noreply.github.com>
2019-08-16 14:19:46 -0400

    TEST

--- src/test/java/dev/dickinson/controllers/BucketTest.java
+++ src/test/java/dev/dickinson/controllers/BucketTest.java
@@ -1,37 +1,62 @@
 package dev.dickinson.controllers;
-
-
 import static org.junit.Assert.fail;
-
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.condition.EnabledIf;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.util.Assert;
-
+import dev.dickinson.services.AmazonClient;
 //import dev.ateam.entities.Build;
 //import dev.ateam.services.BuildService;
-
-
 @ExtendWith(SpringExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 @ContextConfiguration(classes = com.example.demo.MetricsBucketApplication.class)
-
 class BucketTest {
-
-//  @Test
-//  void test() {
-//    fail();
-//  }
-
+  @Autowired
+  private AmazonClient amazonClient;
+  @Test
+  void tokenCreationTest() {
+    try {
+      String verifyData = amazonClient.getCredentials();
+      if (verifyData.contains("{\"arr\":[")){
+        //passes
+      }
+    }
+    catch(Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
 }
+//    
+//    @Nested
+//    @DisplayName("tests that depend on others")
+//    class   
+//  }
+//  
+//  
+//  
+//  @Test
+//  void listAllSpringFilesTest(){
+//    
+//  }
+//  @Test
+//  void listSprintFilesByProject(){
+//    
+//  }
+//  @Test
+//  void listSprintFilesByFileName(){
+//    
+//  }
+  /*
+   * public String listAllSprintFiles() {
+   * return objects.toString();
+   *  public String listSprintFilesByProject(String projectName) {
+   *  return objects.toString();
+   *  public String listSprintFilesByFileName(String fileName) {
+   *  return objects.toString();
+   */
