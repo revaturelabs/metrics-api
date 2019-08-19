cbcda639a58099f14c6abe153b549820f5533fea
Cullen <chaogoesmu@gmail.com>
2019-08-19 09:45:04 -0400

    tests as completed as can be, waiting on product owner

--- src/test/java/dev/dickinson/controllers/BucketTest.java
+++ src/test/java/dev/dickinson/controllers/BucketTest.java
@@ -1,13 +1,20 @@
 package dev.dickinson.controllers;
+
 import static org.junit.Assert.fail;
+
+import java.nio.charset.StandardCharsets;
+
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.multipart.MultipartFile;
 
 import dev.dickinson.services.AmazonClient;
 
@@ -18,51 +25,61 @@ import dev.dickinson.services.AmazonClient;
 @SpringBootTest
 @ContextConfiguration(classes = com.example.demo.MetricsBucketApplication.class)
 class BucketTest {
-  
+
   @Autowired
   private AmazonClient amazonClient;
+
+  MultipartFile test = new MockMultipartFile("files", "filename.txt", "text/plain",
+      "hello".getBytes(StandardCharsets.UTF_8));
+  String testUrl = "";
+
   @Test
   void tokenCreationTest() {
     try {
       String verifyData = amazonClient.getCredentials();
-      if (verifyData.contains("{\"arr\":[")){
-        //passes
+      if (verifyData.contains("{\"arr\":[")) {
+        // passes
       }
-    }
-    catch(Exception e) {
+    } catch (Exception e) {
       e.printStackTrace();
       fail();
     }
   }
-  
-  
-}
-//    
-//    @Nested
-//    @DisplayName("tests that depend on others")
-//    class   
-//  }
-//  
-//  
-//  
-//  @Test
-//  void listAllSpringFilesTest(){
-//    
-//  }
-//  @Test
-//  void listSprintFilesByProject(){
-//    
-//  }
-//  @Test
-//  void listSprintFilesByFileName(){
-//    
-//  }
 
-  /*
-   * public String listAllSprintFiles() {
-   * return objects.toString();
-   *  public String listSprintFilesByProject(String projectName) {
-   *  return objects.toString();
-   *  public String listSprintFilesByFileName(String fileName) {
-   *  return objects.toString();
-   */
+  @Test
+  @Order(1)
+  void uploadFileTest() {
+    testUrl = amazonClient.uploadFile(test);
+    System.out.println(testUrl);
+  }
+
+  @Test
+  @Order(2)
+  void listAllSprintFilesTest() {
+    System.out.println(amazonClient.listAllSprintFiles());
+  }
+
+  @Test
+  @Order(2)
+  void deleteFileTest() {
+    System.out.println(amazonClient.deleteFileFromS3Bucket(testUrl));
+  }
+
+  @Test
+  void listSprintFilesByProjectTest() {
+    System.out.println("listSprintFilesByProjectTest() unimplemented");
+  }
+  
+  @Test
+  void listSprintFilesByFileNameTest() {
+    System.out.println("listSprintFilesByFileNameTest() unimplemented");
+  }
+}
+
+
+/*
+ * public String listAllSprintFiles() { return objects.toString(); public String
+ * listSprintFilesByProject(String projectName) { return objects.toString();
+ * public String listSprintFilesByFileName(String fileName) { return
+ * objects.toString();
+ */
