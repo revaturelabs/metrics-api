7b74a64aac2bcc29fa0892aed894a61448b30580
nicktangmifsud <nicktangmifsud@gmail.com>
2019-08-22 09:49:19 -0400

    started downloadFile

--- src/main/java/com/revature/controllers/BucketController.java
+++ src/main/java/com/revature/controllers/BucketController.java
@@ -1,5 +1,8 @@
 package com.revature.controllers;
 
+import java.io.File;
+import java.net.URL;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -102,6 +105,11 @@ public class BucketController {
     @RequestMapping(value="/countSprintFiles/{projectName}/{sprintName}", method=RequestMethod.GET)
     public int countSprintFiles(@PathVariable String projectName, @PathVariable String sprintName) {
     	return this.amazonClient.countSprintFiles(projectName, sprintName);
+    }
+    
+    @RequestMapping(value="/downloadFile/{projectName}/{sprintName}/{fileName}", method=RequestMethod.GET)
+    public URL downloadFile(@PathVariable String projectName,@PathVariable String sprintName,@PathVariable String fileName) {
+    	return this.amazonClient.downloadFile(projectName, sprintName, fileName);
     }
     
 }
--- src/main/java/com/revature/services/AmazonClient.java
+++ src/main/java/com/revature/services/AmazonClient.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
@@ -19,12 +20,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.HttpMethod;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.ObjectMetadata;
@@ -52,13 +56,13 @@ public class AmazonClient {
 	private String accessKey;
 	@Value("${amazonProperties.secretKey}")
 	private String secretKey;
-	
+
 	private final String reportName = "report";
 
 	@PostConstruct
 	private void initializeAmazon() {
 		AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
-		this.s3client = new AmazonS3Client(credentials);
+		this.s3client = new AmazonS3Client(credentials, new ClientConfiguration().withSignerOverride("AWSS3V4SignerType"));
 	}
 
 	private File convertMultiPartToFile(MultipartFile file) throws IOException {
@@ -111,45 +115,44 @@ public class AmazonClient {
 		ObjectListing ol = s3client.listObjects(bucketName);
 		List<S3ObjectSummary> objects = ol.getObjectSummaries();
 		Set<S3ObjectSummary> sprints = new HashSet<S3ObjectSummary>();
-		
+
 		for (S3ObjectSummary s : objects) {
 			String temp = s.getKey().replaceAll("[^/]", "");
-			if(temp.length() == 2) {
+			if (temp.length() == 2) {
 				sprints.add(s);
 			}
 		}
 
 		return sprints.toString();
 	}
-	
+
 	public String listAllSprintsByProject(String projectName) {
 
 		ObjectListing ol = s3client.listObjects(bucketName);
 		List<S3ObjectSummary> objects = ol.getObjectSummaries();
 		Set<S3ObjectSummary> sprints = new HashSet<S3ObjectSummary>();
-		
+
 		for (S3ObjectSummary s : objects) {
 			String temp = s.getKey().replaceAll("[^/]", "");
-			if(s.getKey().startsWith(projectName) && temp.length() == 2) {
+			if (s.getKey().startsWith(projectName) && temp.length() == 2) {
 				sprints.add(s);
 			}
 		}
 
 		return sprints.toString();
 	}
-	
+
 	public String listAllFilesBySprint(String projectName, String sprintName) {
-		
+
 		ObjectListing ol = s3client.listObjects(bucketName);
 		List<S3ObjectSummary> objects = ol.getObjectSummaries();
 		Set<S3ObjectSummary> sprints = new HashSet<S3ObjectSummary>();
-		
+
 		for (S3ObjectSummary s : objects) {
 			String temp = s.getKey().replaceAll("[^/]", "");
-			if(s.getKey().equals(projectName+"/"+sprintName+"/report/")) {
-				//Do nothing, this is the report file
-			}
-			else if(s.getKey().startsWith(projectName+"/"+sprintName+"/report/") && temp.length() == 3) {
+			if (s.getKey().equals(projectName + "/" + sprintName + "/report/")) {
+				// Do nothing, this is the report file
+			} else if (s.getKey().startsWith(projectName + "/" + sprintName + "/report/") && temp.length() == 3) {
 				sprints.add(s);
 			}
 		}
@@ -169,7 +172,6 @@ public class AmazonClient {
 		return objects.toString();
 	}
 
-
 	public void createProject(String projectName) {
 		try {
 
@@ -177,7 +179,7 @@ public class AmazonClient {
 			ObjectMetadata metadata = new ObjectMetadata();
 			metadata.setContentLength(0);
 
-			s3client.putObject(new PutObjectRequest(bucketName, projectName+"/", input, metadata));
+			s3client.putObject(new PutObjectRequest(bucketName, projectName + "/", input, metadata));
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -193,9 +195,10 @@ public class AmazonClient {
 			ObjectMetadata metadata = new ObjectMetadata();
 			metadata.setContentLength(0);
 
-			s3client.putObject(new PutObjectRequest(bucketName, projectName+"/"+sprintName+"/", input, metadata));
-			
-			s3client.putObject(new PutObjectRequest(bucketName, projectName+"/"+sprintName+"/"+reportName+"/", input, metadata));
+			s3client.putObject(new PutObjectRequest(bucketName, projectName + "/" + sprintName + "/", input, metadata));
+
+			s3client.putObject(new PutObjectRequest(bucketName, projectName + "/" + sprintName + "/" + reportName + "/",
+					input, metadata));
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -209,7 +212,7 @@ public class AmazonClient {
 		try {
 			File file = convertMultiPartToFile(multipartFile);
 			String fileName = generateFileName(multipartFile);
-			fileUrl = projectName+"/" +sprintName+"/" + reportName+"/" + fileName;
+			fileUrl = projectName + "/" + sprintName + "/" + reportName + "/" + fileName;
 			uploadFileTos3bucket(fileUrl, file);
 			file.delete();
 		} catch (Exception e) {
@@ -220,75 +223,129 @@ public class AmazonClient {
 	}
 
 	public void uploadMultipleFiles(String projectName, String sprintName, MultipartFile[] file) {
-		
-		for(MultipartFile f: file) {
-			uploadReportFile(projectName,sprintName,f);
+
+		for (MultipartFile f : file) {
+			uploadReportFile(projectName, sprintName, f);
 		}
 	}
-	
+
 	public void deleteProject(String projectName) {
 		ObjectListing ol = s3client.listObjects(bucketName);
 		List<S3ObjectSummary> objects = ol.getObjectSummaries();
-		
-		for(S3ObjectSummary s : objects) {
-			if(s.getKey().startsWith(projectName + "/")) {
+
+		for (S3ObjectSummary s : objects) {
+			if (s.getKey().startsWith(projectName + "/")) {
 				s3client.deleteObject(new DeleteObjectRequest(bucketName, s.getKey()));
 			}
 		}
-		
+
 	}
-	
+
 	public void deleteSprint(String projectName, String sprintName) {
 		ObjectListing ol = s3client.listObjects(bucketName);
 		List<S3ObjectSummary> objects = ol.getObjectSummaries();
-		
-		for(S3ObjectSummary s : objects) {
-			if(s.getKey().startsWith(projectName + "/" + sprintName + "/")) {
+
+		for (S3ObjectSummary s : objects) {
+			if (s.getKey().startsWith(projectName + "/" + sprintName + "/")) {
 				s3client.deleteObject(new DeleteObjectRequest(bucketName, s.getKey()));
 			}
 		}
 	}
-	
+
 	public int countSprints(String projectName) {
 		int count = 0;
 
 		ObjectListing ol = s3client.listObjects(bucketName);
 		List<S3ObjectSummary> objects = ol.getObjectSummaries();
-		
+
 		for (S3ObjectSummary s : objects) {
 			String temp = s.getKey().replaceAll("[^/]", "");
-			if(s.getKey().startsWith(projectName) && temp.length() == 2) {
+			if (s.getKey().startsWith(projectName) && temp.length() == 2) {
 				count++;
 			}
 		}
-		
+
 		return count;
 	}
-	
+
 	public int countSprintFiles(String projectName, String sprintName) {
 		int count = 0;
-		
-		
+
 		ObjectListing ol = s3client.listObjects(bucketName);
 		List<S3ObjectSummary> objects = ol.getObjectSummaries();
-		
+
 		for (S3ObjectSummary s : objects) {
 			String temp = s.getKey().replaceAll("[^/]", "");
-			
-			if(s.getKey().equals(projectName+"/"+sprintName+"/report/")) {
-				//Do nothing, this is the report file
-			}
-			else if(s.getKey().startsWith(projectName+"/"+sprintName+"/report/") && temp.length() == 3) {
+
+			if (s.getKey().equals(projectName + "/" + sprintName + "/report/")) {
+				// Do nothing, this is the report file
+			} else if (s.getKey().startsWith(projectName + "/" + sprintName + "/report/") && temp.length() == 3) {
 				count++;
 			}
 		}
-		
+
 		return count;
 	}
-	
+
 	public String deleteFileFromS3Bucket(String projectName, String sprintName, String fileName) {
-		s3client.deleteObject(new DeleteObjectRequest(bucketName, projectName+"/"+sprintName+"/report/"+fileName));
+		s3client.deleteObject(
+				new DeleteObjectRequest(bucketName, projectName + "/" + sprintName + "/report/" + fileName));
 		return "Successfully deleted";
 	}
+
+	public URL downloadFile(String projectName, String sprintName, String fileName) {
+		
+		GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                new GeneratePresignedUrlRequest(bucketName, projectName+"/"+sprintName+"/report/"+fileName)
+                        .withMethod(HttpMethod.GET)
+                        .withExpiration(null);
+        URL url = s3client.generatePresignedUrl(generatePresignedUrlRequest);
+        System.out.println(url);
+        return url;
+		
+	}
+////        String bucketName = "metricbuckettest";
+//		
+//		projectName = "Project1";
+//		sprintName = "Sprint1";
+//		fileName = "derserserterstferl.txt";
+//		
+//		File newFile = new File(fileName);
+//
+//		try {
+////            S3Object o = s3client.getObject(new GetObjectRequest(bucketName, folderPath + "/" + fileName));
+//			S3Object o = s3client.getObject(
+//					new GetObjectRequest(bucketName, projectName + "/" + sprintName + "/report/" + fileName));
+//			S3ObjectInputStream s3is = o.getObjectContent();
+//			
+//			System.out.println(o);
+//			
+////			File newFile = new File(fileName);
+//			
+//			
+//			// Save the file to the Desktop
+////			FileOutputStream fos = new FileOutputStream(new File(fileName));
+//			FileOutputStream fos = new FileOutputStream(newFile);
+//
+//			byte[] read_buf = new byte[1024];
+//			int read_len = 0;
+//			while ((read_len = s3is.read(read_buf)) > 0) {
+//				
+//				fos.write(read_buf, 0, read_len);
+//			}
+//			s3is.close();
+//			fos.close();
+//		} catch (AmazonServiceException e) {
+//			System.err.println(e.getErrorMessage());
+//		} catch (FileNotFoundException e) {
+//			System.err.println(e.getMessage());
+//		} catch (IOException e) {
+//			System.err.println(e.getMessage());
+//		}
+//
+//		return newFile;
+////		return new File("This is not the actual file");
+//	}
+	
 	
 }
